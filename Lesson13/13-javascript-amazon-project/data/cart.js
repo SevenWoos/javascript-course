1200e343233f279a554ee577f1fc4d985fb46c5d
SevenWoos <swenwu96@gmail.com>
2026-02-14 11:09:36 -0800

    Move addToCart into cart.js

--- Lesson13/13-javascript-amazon-project/data/cart.js
+++ Lesson13/13-javascript-amazon-project/data/cart.js
@@ -1,1 +1,28 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+    // Check for duplicate items do we can increase quantity
+    cart.forEach((cartItem) => {
+      if(productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+    const quantity = Number(quantitySelector.value);
+
+    if(matchingItem) {
+      // matchingItem.quantity+=1;
+      matchingItem.quantity+=quantity;
+    }
+    else {
+      cart.push({
+        productId: productId, 
+        quantity: quantity
+      });
+    }
+};
