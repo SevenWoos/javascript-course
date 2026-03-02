2425c611620ea1006037d60a5be919c8a3159abc
SevenWoos <swenwu96@gmail.com>
2026-03-02 12:49:40 -0800

    15i. Update Cart Quantity in paymentSummary.

--- Lesson13/13-javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ Lesson13/13-javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -20,6 +20,14 @@ export function renderPaymentSummary() {
   const taxCents = totalBeforeTaxCents * 0.1;
   const totalCents = totalBeforeTaxCents + taxCents;
 
+
+  // 15i. Update Cart Quantity in paymentSummary.
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
   // View(generate the HTML)
   const paymentSummaryHTML = `
     <div class="payment-summary-title">
@@ -27,7 +35,7 @@ export function renderPaymentSummary() {
     </div>
 
     <div class="payment-summary-row">
-      <div>Items (3):</div>
+      <div>Items (${cartQuantity}):</div>
       <div class="payment-summary-money">
         $${formatCurrency(productPriceCents)}
       </div>
