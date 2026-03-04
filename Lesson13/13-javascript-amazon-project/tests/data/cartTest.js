3a70aae34a9c9d784c643a0fa281e4b113e036b9
SevenWoos <swenwu96@gmail.com>
2026-03-03 22:43:08 -0800

    16e. beforeEach hook to setup localStorage and setItem.

--- Lesson13/13-javascript-amazon-project/tests/data/cartTest.js
+++ Lesson13/13-javascript-amazon-project/tests/data/cartTest.js
@@ -1,10 +1,14 @@
 import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
-  it('adds an existing product to the cart', () => {
+  // beforeEach() hook
+  beforeEach(() => {
+    // Mocks = lets you replace method with a fake version. Use spyOn.
     // Mock 'setItem' so our specs do not affect the actual 'cart'.
     spyOn(localStorage, 'setItem');
+  });
 
+  it('adds an existing product to the cart', () => {
     // Mock localStorage to already have an existing product.
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
@@ -31,11 +35,6 @@ describe('test suite: addToCart', () => {
   });
 
   it('adds a new product to the cart', () => {
-    // Mocks = lets you replace method with a fake version. Use spyOn.
-
-    // Mock 'setItem' so our specs do not affect the actual 'cart'.
-    spyOn(localStorage, 'setItem');
-
     // Create fake version of 'getItem' we can customize.
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
@@ -53,7 +52,7 @@ describe('test suite: addToCart', () => {
       quantity: 1, 
       deliveryOptionId: '1'
     }]));
-    
+
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
   });
