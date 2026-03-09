0b9be865e80288ce3fe7b399678049b777bea120
SevenWoos <swenwu96@gmail.com>
2026-03-09 13:41:53 -0700

    17c

--- Lesson17/data/17a.js
+++ Lesson17/data/17a.js
@@ -1,6 +1,7 @@
 class Car {
   brand;
   model;
+  speed = 0;
 
   constructor(carDetails) {
     this.brand = carDetails.brand;
@@ -8,8 +9,28 @@ class Car {
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}`);
+    console.log(`${this.brand} ${this.model}, Speed ${this.speed} km/h`);
   };
+
+  // Increases speed by 5
+  go() {
+    this.speed += 5;
+
+    // Limit speed to 200
+    if (this.speed > 200) {
+      this.speed = 200;
+    };
+  }
+
+  // Decreases speed by 5
+  brake() {
+    this.speed -= 5;
+
+    // Limit the speed to 0.
+    if (this.speed < 0) {
+      this.speed = 0;
+    };
+  }
 }
 
 const car1 = new Car({
@@ -26,4 +47,14 @@ console.log(car1);
 console.log(car2);
 
 car1.displayInfo();
+car1.go();
+car1.go();
+car1.go();
+car1.brake();
+car1.displayInfo();
+
+car2.displayInfo();
+car2.go();
+car2.brake();
+car2.brake();
 car2.displayInfo();
