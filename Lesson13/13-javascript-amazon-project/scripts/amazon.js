f95c766ff26e2df39bc20ed5c9417095e71597c9
SevenWoos <swenwu96@gmail.com>
2026-03-17 13:25:49 -0700

    Load products from the backend.

--- Lesson13/13-javascript-amazon-project/scripts/amazon.js
+++ Lesson13/13-javascript-amazon-project/scripts/amazon.js
@@ -1,112 +1,117 @@
 import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
-let productsHTML = '';
+// Load products using backend.
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-    <div class="product-image-container">
-      <img class="product-image"
-        src="${product.image}">
-    </div>
+function renderProductsGrid() {
+  let productsHTML = '';
 
-    <div class="product-name limit-text-to-2-lines">
-      ${product.name}
-    </div>
-
-    <div class="product-rating-container">
-      <img class="product-rating-stars"
-        src="${product.getStarsURL()}">
-      <div class="product-rating-count link-primary">
-        ${product.rating.count}
+  products.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image"
+          src="${product.image}">
       </div>
+
+      <div class="product-name limit-text-to-2-lines">
+        ${product.name}
+      </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+          src="${product.getStarsURL()}">
+        <div class="product-rating-count link-primary">
+          ${product.rating.count}
+        </div>
+      </div>
+
+      <div class="product-price">
+        ${product.getPrice()}
+      </div>
+
+      <div class="product-quantity-container">
+        <select class="js-quantity-selector-${product.id}">
+          <option selected value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      ${product.extraInfoHTML()}
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart js-added-to-cart-${product.id}">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary js-add-to-cart" 
+      data-product-id="${product.id}">
+        Add to Cart
+      </button>
     </div>
+    `;
 
-    <div class="product-price">
-      ${product.getPrice()}
-    </div>
-
-    <div class="product-quantity-container">
-      <select class="js-quantity-selector-${product.id}">
-        <option selected value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-      </select>
-    </div>
-
-    ${product.extraInfoHTML()}
-
-    <div class="product-spacer"></div>
-
-    <div class="added-to-cart js-added-to-cart-${product.id}">
-      <img src="images/icons/checkmark.png">
-      Added
-    </div>
-
-    <button class="add-to-cart-button button-primary js-add-to-cart" 
-    data-product-id="${product.id}">
-      Add to Cart
-    </button>
-  </div>
-  `;
-
-});
-
-// Data Attribute has to start with data and be separated with dashes(kebab style)
-// The kebab case gets CONVERTED to CAMEL CASE
-
-document.querySelector('.js-products-grid')
-  .innerHTML = productsHTML;
-
-
-function updateCartQuantity(productId) {
-  const cartQuantity = calculateCartQuantity();
-
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-
-  const addedMessage = document.querySelector(
-    `.js-added-to-cart-${productId}`
-  );
-
-  addedMessage.classList.add('added-to-cart-visible');
-
-  setTimeout(() => {
-    addedMessage.classList.remove('added-to-cart-visible');
-  }, 2000);
-};
-
-// Using 14d function
-updatePageCartQuantity();
-
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      // The kebab case gets CONVERTED to CAMEL CASE
-      const productId = button.dataset.productId;
-
-      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-      const quantity = Number(quantitySelector.value);
-
-      addToCart(productId, quantity);
-      updateCartQuantity(productId);
-    });
   });
 
+  // Data Attribute has to start with data and be separated with dashes(kebab style)
+  // The kebab case gets CONVERTED to CAMEL CASE
 
-// 14d. Update the Amazon Page CartQuantity when it loads. New function that does take in productId parameter.
-function updatePageCartQuantity() {
-  const cartQuantity = calculateCartQuantity();
+  document.querySelector('.js-products-grid')
+    .innerHTML = productsHTML;
 
-  document.querySelector('.js-page-cart-quantity')
-    .innerHTML = cartQuantity;
+
+  function updateCartQuantity(productId) {
+    const cartQuantity = calculateCartQuantity();
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+
+    addedMessage.classList.add('added-to-cart-visible');
+
+    setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+  };
+
+  // Using 14d function
+  updatePageCartQuantity();
+
+  document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+      button.addEventListener('click', () => {
+        // The kebab case gets CONVERTED to CAMEL CASE
+        const productId = button.dataset.productId;
+
+        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+        const quantity = Number(quantitySelector.value);
+
+        addToCart(productId, quantity);
+        updateCartQuantity(productId);
+      });
+    });
+
+
+  // 14d. Update the Amazon Page CartQuantity when it loads. New function that does take in productId parameter.
+  function updatePageCartQuantity() {
+    const cartQuantity = calculateCartQuantity();
+
+    document.querySelector('.js-page-cart-quantity')
+      .innerHTML = cartQuantity;
+  };
 };
